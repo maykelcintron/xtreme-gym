61742c1f3450c9a2dd92d0d6cff6c538ddaeac8d
Jhonx007 <molinaalvarezjhongregory@gmail.com>
2026-01-09 00:54:02 -0400

    Config routes

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const navItems = [
   },
   {
     label: "Inventario",
-    path: "/dashboard/inventary",
+    path: "/dashboard/inventory",
     icon: "M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4",
   },
   {
