381c8b6d531095710a075788f30b23fb7dd04535
Maykel Cintron <tecnopcell211000@gmail.com>
2026-01-10 00:42:35 -0400

    fix: role permission

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,12 +3,14 @@ import { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface User {
     role: string;
+    permission: string;
   }
 
   interface Session {
     user: {
       id: string;
       role: string;
+      permission: string;
     } & DefaultSession["user"];
   }
 }
@@ -16,11 +18,13 @@ declare module "next-auth" {
 declare module "next-auth/jwt" {
   interface JWT {
     role: string;
+    permission: string;
   }
 }
 
 declare module "@auth/core/adapters" {
   interface AdapterUser {
     role: string;
+    permission: string;
   }
 }
